3c50adf30ac8315d702787b1854721cbf35a152d
Swarna Jang Maden <limbujungmaden@gmail.com>
2023-12-01 21:40:06 +0545

    create custom validation: unique email

--- model/User.js
+++ model/User.js
@@ -3,7 +3,20 @@ const Schema = mongoose.Schema;
 
 const UserSchema = new Schema({
   name: { type: String, required: true, minLength: 3 },
-  email: { type: String, required: true },
+  email: {
+    type: String,
+    required: true,
+    validate: {
+      validator: async function (requestValue) {
+        let user = await mongoose.models.Users.findOne({ email: requestValue });
+        if (user) {
+          return false;
+        }
+        return true;
+      },
+      message: "email already in use",
+    },
+  },
   password: { type: String, required: true },
   contact: { type: Number },
   role: { type: String, enum: ["company", "job-seeker"] },
